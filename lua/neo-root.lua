69c2933d1dda3e499be802a6ed8e54912e522759
Ning Wang <nyngwang@gmail.com>
2022-01-25 04:24:38 +0800

    Fix Bug: `NeoRootChange` got rekt on `./`-prefixed path (#5)

--- lua/neo-root.lua
+++ lua/neo-root.lua
@@ -6,30 +6,11 @@ local EXPR_NOREF_NOERR_TRUNC = { expr = true, noremap = true, silent = true, now
 RED_PILL = 1
 BLUE_PILL = 2
 -- globals
-NEO_ZOOM_DID_INIT = false
-CUR_MODE = nil
-PROJ_ROOT = nil
-USER_ROOT = ''
+CUR_MODE = RED_PILL
+_PROJ_ROOT = vim.fn.getcwd()
+PROJ_ROOT = vim.fn.getcwd() -- this cannot be relative path!
 
 local M = {}
-
-local function init()
-  CUR_MODE = RED_PILL
-  -- NOTE: Both `~`, `-`, `..` works with `vim.cmd`
-  PROJ_ROOT = vim.fn.getcwd()
-end
-
-local function execute_mode_behaviour()
-  if CUR_MODE == RED_PILL then
-    vim.api.nvim_set_current_dir(vim.fn.expand('%:p:h'))
-  else -- CUR_MODE == BLUE_PILL
-    if USER_ROOT ~= '' then
-      vim.api.nvim_set_current_dir(USER_ROOT)
-    else
-      vim.api.nvim_set_current_dir(PROJ_ROOT)
-    end
-  end
-end
 
 local function apply_change()
   M.execute()
@@ -37,17 +18,17 @@ local function apply_change()
 end
 ---------------------------------------------------------------------------------------------------
 function M.execute()
-  if not NEO_ZOOM_DID_INIT then
-    init()
-    NEO_ZOOM_DID_INIT = true
-  end
   -- NOTE: Don't use `string.find` to compare type, since empty string `''` will always match
   -- NOTE: Don't use `vim.opt.filetype`, since everyone set it locally.
   if vim.bo.buftype ~= "terminal" -- TODO: should be customizable
     and vim.bo.filetype ~= "dashboard"
     and vim.bo.filetype ~= "NvimTree"
     and vim.bo.filetype ~= "FTerm" then
-    execute_mode_behaviour()
+    if CUR_MODE == RED_PILL then
+      vim.cmd('cd ' .. vim.fn.expand('%:p:h'))
+    else -- CUR_MODE == BLUE_PILL
+      vim.cmd('cd ' .. PROJ_ROOT)
+    end
   end
 end
 
@@ -61,12 +42,25 @@ function M.change_mode()
 end
 
 function M.change_project_root()
-  USER_ROOT = vim.fn.input('Set Project Root: ')
-  if USER_ROOT ~= '' then -- reset
-    vim.cmd('cd ' .. USER_ROOT)
-  else
-    vim.cmd('cd ' .. PROJ_ROOT)
+  local input = vim.fn.input('Set Project Root: ')
+  if (input == '' or input:match('%s+')) then -- reset signal
+    PROJ_ROOT = _PROJ_ROOT
+    apply_change()
+    return
   end
+
+  if input:sub(1,2) == './' or input:sub(1,3) == '../' then -- relative path
+    local cwd = vim.fn.getcwd()
+    if cwd:sub(-1) == '/' then
+      vim.cmd('cd ' .. cwd ..  input)
+    else
+      vim.cmd('cd ' .. cwd .. '/' .. input)
+    end
+  else -- the last case `~`
+    vim.cmd('cd ' .. input)
+  end
+  -- `PROJ_ROOT` only store normalized result
+  PROJ_ROOT = vim.fn.getcwd()
   apply_change()
 end
 
